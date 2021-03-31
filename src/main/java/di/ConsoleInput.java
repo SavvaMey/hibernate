f2df92314579e44479d080c9001701fd7b52b51e
SavvaMey <savva28031997@gmail.com>
2021-03-31 17:33:08 +0300

    0. Что такое DI? [#474437]

---
+++ src/main/java/di/ConsoleInput.java
@@ -0,0 +1,16 @@
+package di;
+
+import java.util.Scanner;
+
+public class ConsoleInput {
+    private Scanner scanner = new Scanner(System.in);
+
+    public String askStr(String question) {
+        System.out.print(question);
+        return scanner.nextLine();
+    }
+
+    public int askInt(String question) {
+        return Integer.parseInt(askStr(question));
+    }
+}
